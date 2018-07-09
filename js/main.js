a3e230c73a2c4f00156d07a783a4f0a22660ee29
Petar Marjanovic <petarmarjanovic12@yahoo.com>
2018-07-09 15:54:53 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// const navigation = document.querySelector('.navigation');
+// const sideBar = document.querySelector('.social');
+
+// let navHeight = navigation.clientHeight;
+// let windowHeight = window.innerHeight;
+// let sideBarHeight = windowHeight - navHeight;
+
+// sideBar.style.height = sideBarHeight + 'px';
+// console.log(window)
