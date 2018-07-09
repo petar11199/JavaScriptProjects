51d3586e5cbfd06fe7dda52506b7a15559fe3793
Petar Marjanovic <petarmarjanovic12@yahoo.com>
2018-07-09 17:49:42 +0200

    added animations

--- js/main.js
+++ js/main.js
@@ -7,3 +7,16 @@
 
 // sideBar.style.height = sideBarHeight + 'px';
 // console.log(window)
+
+const projects = document.querySelectorAll('.project');
+
+function animateProject(e) {
+    if (this.classList.contains('active')) {
+        this.classList.remove('active');
+    } else {
+        this.classList.add('active');
+    }
+}
+
+projects.forEach(project => project.addEventListener('mouseenter', animateProject));
+projects.forEach(project => project.addEventListener('mouseleave', animateProject));
